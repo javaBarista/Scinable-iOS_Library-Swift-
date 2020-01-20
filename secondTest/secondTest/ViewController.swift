2fa024d4ac1cb676d07fc1c1ca10157b6513eab1
javaBarista <nobles1030@gmail.com>
2020-01-20 14:31:50 +0900

    Swift Code Project에서 테스트

---
+++ secondTest/secondTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  secondTest
+//
+//  Created by 株式会社シナブル on 2019/09/17.
+//  Copyright © 2019 株式会社シナブル. All rights reserved.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+}
+
